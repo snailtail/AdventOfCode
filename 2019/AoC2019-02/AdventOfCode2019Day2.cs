c11c86f64ec84b619c5044cb5e63cd584177a90a
Magnus Olofsson <magnusolofsson@outlook.com>
2025-10-06 20:36:05 +0200

    Updates

---
+++ 2019/AoC2019-02/AdventOfCode2019Day2.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.TwentyNineteen.Day02
+{
+    class Day2
+    {
+        /**
+        Take a string with "integer" separated by commas.
+        Position 0 is opcode
+        Position 1 is where to read input1 from
+        Position 2 is where to read input2 from
+        Position 3 is where to store results of operation
+
+        Opcode 1 is add
+        Opcode 2 is multiply
+        Opcode 99 is halt immediately
+
+
+
+        **/
+        public int FinishedResult { get; set; }
+
+        public Day2(string InputString, int x=-1, int y=-1)
+        {
+            
+            var stringIntArr = InputString.Split(',');
+            if (x > -1)
+            {
+                stringIntArr[1] = x.ToString();
+            }
+            if (y > -1)
+            {
+                stringIntArr[2] = y.ToString();
+            }
+            //Console.WriteLine($"Running with x={x}, y={y}");
+            //Console.WriteLine("Number of positions in input: {0}", stringIntArr.Length);
+            int op_pos = 0;
+            for(op_pos=0;op_pos<= (stringIntArr.Length-5); op_pos+=4)
+            {
+                var opCode = int.Parse(stringIntArr[op_pos]);
+                var result = 0;
+                //Console.WriteLine("Position {0}: {1} {2} {3} {4}", op_pos+1, stringIntArr[op_pos], stringIntArr[op_pos+1], stringIntArr[op_pos+2], stringIntArr[op_pos+3]);
+                if (opCode == 99)
+                {
+                    //Console.WriteLine("OPCODE 99 EXITING");
+                    return;
+                }
+                else if(opCode==1)
+                {
+                    // Operation = add
+                    //Console.WriteLine("OpCode 1, adding.");
+                    var input1 = int.Parse(stringIntArr[int.Parse(stringIntArr[op_pos + 1])]);
+                    var input2 = int.Parse(stringIntArr[int.Parse(stringIntArr[op_pos + 2])]);
+                    result =  input1 + input2 ;
+                }
+                else if(opCode==2)
+                {
+                    //Console.WriteLine("OpCode 2, multiplying.");
+                    var input1 = int.Parse(stringIntArr[int.Parse(stringIntArr[op_pos + 1])]);
+                    var input2 = int.Parse(stringIntArr[int.Parse(stringIntArr[op_pos + 2])]);
+                    result = input1 * input2;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect opcode {0}", opCode);
+                    return;
+                }
+                var storePos = int.Parse(stringIntArr[op_pos + 3]);
+                //Console.WriteLine("Trying to store result {0} in stringIntArr[{1}]", result.ToString(), stringIntArr[storePos]);
+                
+                stringIntArr[storePos] = result.ToString();
+                //Console.WriteLine(stringIntArr[storePos]);
+                //Console.WriteLine("***********************");
+                FinishedResult = int.Parse(stringIntArr[0]);
+            }
+            
+
+        }
+    }
+}
---
+++ 2019/AoC2019-02/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.TwentyNineteen.Day02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputValues = "1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,1,19,9,23,1,23,6,27,2,27,13,31,1,10,31,35,1,10,35,39,2,39,6,43,1,43,5,47,2,10,47,51,1,5,51,55,1,55,13,59,1,59,9,63,2,9,63,67,1,6,67,71,1,71,13,75,1,75,10,79,1,5,79,83,1,10,83,87,1,5,87,91,1,91,9,95,2,13,95,99,1,5,99,103,2,103,9,107,1,5,107,111,2,111,9,115,1,115,6,119,2,13,119,123,1,123,5,127,1,127,9,131,1,131,10,135,1,13,135,139,2,9,139,143,1,5,143,147,1,13,147,151,1,151,2,155,1,10,155,0,99,2,14,0,0";
+            int d2step2wantedResult = 19690720;
+            bool bFound = false;
+
+            var day2Step1 = new Day2(inputValues);
+            Console.WriteLine("Day 2 step 1 result: {0}", day2Step1.FinishedResult);
+
+
+
+
+            for (int x = 0; x < 100; x++)
+            {
+                for (int y = 0; y < 100; y++)
+                {
+                    var testDay = new Day2(inputValues, x, y);
+                    if (testDay.FinishedResult == d2step2wantedResult)
+                    {
+                        //Console.WriteLine("X: {0}\tY: {1}", x, y);
+                        Console.WriteLine("Day 2 step 2 result: {0}", (100 * x) + y);
+                        bFound = true;
+                        break;
+                    }
+
+                }
+                if (bFound)
+                    break;
+            }
+
+        }
+    }
+}
