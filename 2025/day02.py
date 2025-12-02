6636004541a5b8d0c69dd6661d8d9cd3795d3251
Magnus Olofsson <magnusolofsson@outlook.com>
2025-12-02 17:03:41 +0100

    Cleaning up and adding docstrings

--- 2025/day02.py
+++ 2025/day02.py
@@ -7,16 +7,20 @@ def setup(path):
         
 
 def part1(data):
+    """finds the invalid IDs by looking for any ID which is made only of some sequence of digits repeated twice"""
     invalid_ids = []
     for r in data:
         seq = range(r[0],r[1]+1)
         for v in seq:
             s = str(v)
+            if len(s) % 2 != 0:
+                continue
             if s[:len(s)//2] == s[len(s)//2:]:
                 invalid_ids.append(v)
     return invalid_ids
 
 def part2(data):
+    """Looking for invalid IDs by checking if it is made only of some sequence of digits repeated at least twice"""
     invalid_ids = []
     for r in data:
         seq = range(r[0],r[1]+1)
@@ -26,13 +30,12 @@ def part2(data):
                 part = s[:le]
                 if len(part) * s.count(part) == len(s):
                     invalid_ids.append(v)
-                    #print(v)
                     break
     return invalid_ids
 
 if __name__ == "__main__":
     
-    data = setup("testinput_day02.dat")
+    data = setup("input_day02.dat")
     p1_result = sum(part1(data))
     print(p1_result)
     p2_result = sum(part2(data))
