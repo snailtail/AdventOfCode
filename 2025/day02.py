53b444674aff512757b118eb87dc362f268eb8c2
Magnus Olofsson <magnusolofsson@outlook.com>
2025-12-02 22:07:06 +0100

    Rename variables for a bit more clarity. And add docstring for setup()

--- 2025/day02.py
+++ 2025/day02.py
@@ -1,41 +1,47 @@
 """ Day 2: Gift Shop """
 
 def setup(path):
+    """
+    Parses input and returns a list of tuples. Each tuple represents a range (from,to) The to-value is to be treated as inclusive
+    Example: [(11, 22), (95, 115), (998, 1012), (1188511880, 1188511890), (222220, 222224), (1698522, 1698528), (446443, 446449), (38593856, 38593862), (565653, 565659), (824824821, 824824827), (2121212118, 2121212124)]
+    """
     with open(path, "r") as f:
-        input = f.readline().strip().split(",")
-        return [tuple(int(x) for x in s.split("-")) for s in input]
+        input = f.read().strip().split(",")
+        return [tuple(int(value) for value in rangestring.split("-")) for rangestring in input]
         
 
 def part1(data):
     """finds the invalid IDs by looking for any ID which is made only of some sequence of digits repeated twice"""
     invalid_ids = []
-    for r in data:
-        seq = range(r[0],r[1]+1)
-        for v in seq:
-            s = str(v)
-            if len(s) % 2 != 0:
+    for range_tuple in data:
+        seq = range(range_tuple[0],range_tuple[1]+1)
+        for number in seq:
+            string_of_value = str(number)
+            if len(string_of_value) % 2 != 0:
                 continue
-            if s[:len(s)//2] == s[len(s)//2:]:
-                invalid_ids.append(v)
+            if string_of_value[:len(string_of_value)//2] == string_of_value[len(string_of_value)//2:]:
+                invalid_ids.append(number)
     return invalid_ids
 
 def part2(data):
-    """Looking for invalid IDs by checking if it is made only of some sequence of digits repeated at least twice"""
+    """
+    Looking for invalid IDs by checking if it is made only of some sequence of digits repeated at least twice
+    """
     invalid_ids = []
-    for r in data:
-        seq = range(r[0],r[1]+1)
-        for v in seq:
-            s = str(v)
-            for le in range(1,len(s)):
-                part = s[:le]
-                if len(part) * s.count(part) == len(s):
-                    invalid_ids.append(v)
+    for range_tuple in data:
+        seq = range(range_tuple[0],range_tuple[1]+1)
+        for number in seq:
+            string_of_value = str(number)
+            for le in range(1,len(string_of_value)):
+                part = string_of_value[:le]
+                if len(part) * string_of_value.count(part) == len(string_of_value):
+                    invalid_ids.append(number)
                     break
     return invalid_ids
 
 if __name__ == "__main__":
     
-    data = setup("input_day02.dat")
+    data = setup("testinput_day02.dat")
     p1_result = sum(part1(data))
     print(p1_result)
     p2_result = sum(part2(data))
