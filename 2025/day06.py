73ae5c332b2743ad81e0eef74c54294cd7dae4ea
Magnus Olofsson <magnusolofsson@outlook.com>
2025-12-06 05:52:47 +0100

    Add setup

--- 2025/day06.py
+++ 2025/day06.py
@@ -1,2 +1,17 @@
 """
+    Day 6: 
 """
+
+def setup(path = "testinput_day06.dat"):
+    """
+        Parses input and ...
+        Example: 
+    """
+    with open(path,"r") as f:
+        input = f.read().splitlines()
+    
+    return input
+
+if __name__ == "__main__":
+    data = setup("testinput_day06.dat")
+    print(data)
